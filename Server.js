1289eec3cece22acc815fe6feae8ab0b3e216811
Madhavan C <139116570+madhavan521@users.noreply.github.com>
2024-09-01 10:24:51 +0530

    Server.js

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express =require('express')
 const app=express()
 require('dotenv').config()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8000
 const cors = require('cors')
 const headerrouter= require('./Router/HeaderRouter')
 const catagoryrouter =require('./Router/CatagoryRouter')
